253697f870128889c77c720ca8632b1959805261
Christian D. Lamothe <c.lamothe25@gmail.com>
2017-10-15 00:04:43 +0000

    completed day8.js

--- day8.js
+++ day8.js
@@ -1,7 +1,34 @@
 function processData(input) {
-    
-    
     //Enter your code here
+    input = input.split('\n');
+    
+    //convert input[0] to integer
+    var int = parseInt(input[0]);
+    
+    //slice input into 2 arrays
+    var entries = input.slice(1, int + 1);
+    var queries = input.slice(int + 1);
+   // console.log(entries);
+    //console.log(queries);
+    
+    //declare phoneBook object
+    var phoneBook = {};
+    
+    //to populate phoneBook object
+    for (var i = 0; i < entries.length; i++) {
+        var newEntries = entries[i].split(" ");
+        //console.log(newEntries);
+        phoneBook [newEntries[0]] = newEntries[1];
+    }
+    
+    //to check if query names are in the phoneBook object
+   for (var i = 0; i < queries.length; i++) {
+       if (phoneBook.hasOwnProperty(queries[i])) {
+           console.log(queries[i] + '=' + phoneBook[queries[i]]);
+           } else {
+               console.log('Not found');
+           }
+   }
 } 
 
 
